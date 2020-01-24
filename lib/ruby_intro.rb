fd1600f4bf4a372d2aa9cd3b262e64c89c6fdaec
EC2 Default User <ec2-user@ip-172-31-47-249.ec2.internal>
2020-01-24 06:12:28 +0000

    Complete part 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,28 +4,71 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  arr.each { |x| sum += x }
+  return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+  arr.sort!.reverse!
+  return (arr[0] + arr[1])
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.length < 1
+    return false
+  end
+  i = 0
+  while (i < arr.length)
+    j = i + 1
+    while (j < arr.length)
+      if arr[i] + arr[j] == n
+        return true
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s.length == 0 || !s[0].match(/^[[:alpha:]]$/)
+    return false
+  end
+  if (s[0] == "A" || s[0] == "E" || s[0] == "I" || s[0] == "O" || s[0] == "U" || 
+    s[0] == "a" || s[0] == "e" || s[0] == "i" || s[0] == "o" || s[0] == "u" )
+    return false
+  end 
+  return true
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s.count('01') != s.size
+    return false
+  end
+  if s[s.length - 1] == '0' && s[s.length - 2] == '0'
+    return true
+  end
+
+  return false
+  
 end
 
 # Part 3
@@ -33,3 +76,5 @@ end
 class BookInStock
 # YOUR CODE HERE
 end
+
+puts binary_multiple_of_4?("0")
